a692abb4ebee1d87021a79d296d3c235504f0bb9
chase <student@epicodus.com>
2015-05-01 10:11:43 -0700

    create 'all' class method and return empty array, spec pass

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -3,7 +3,7 @@ require('dictionary')
 require('word')
 require('definition')
 
-describe('Dictionary') do
+describe('Definition') do
 
   describe('#define') do
    it('returns the definition of a word') do
@@ -11,4 +11,17 @@ describe('Dictionary') do
      expect(test_word.define()).to(eq("test_word is a Ruby argument"))
    end
  end
+
+ describe('.all') do
+   it("is an empty array at first") do
+     expect(Definition.all()).to(eq([]))
+   end
+ end
+
+ # describe('#save') do
+ #   it('adds a definition to the array of saved definitions') do
+ #     test_word = Definition.new("test_word is a Ruby argument")
+ #     expect(Definition.all()).to(eq([test_word]))
+ #   end
+ # end
  end
