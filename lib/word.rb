c8ac7ffcdee00ce21cccfd18595c8b75b83d352a
chase <student@epicodus.com>
2015-05-01 11:10:16 -0700

    return an empty array to store word in, spec pass

--- lib/word.rb
+++ lib/word.rb
@@ -1,5 +1,5 @@
 class Word
-
+@@words = []
   define_method(:initialize) do |new_word|
     @new_word = new_word
   end
@@ -7,4 +7,9 @@ class Word
   define_method(:new_word) do
     @new_word
   end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -11,4 +11,43 @@ describe('Word') do
     expect(test_word.new_word()).to(eq("cat"))
     end
   end
+  # describe('#save') do
+  #   it('adds a word to the array of saved words') do
+  #     test_word = Word.new("cat")
+  #     test_word.save()
+  #     expect(Word.all()).to(eq([test_word]))
+  #   end
+  # end
+  #
+  # describe('#id') do
+  #   it("returns the id of the definition") do
+  #     test_word = Definition.new("test_word is a Ruby argument")
+  #     test_word.save()
+  #     expect(test_word.id()).to(eq(1))
+  #   end
+  # end
+
+   describe('.all') do
+     it("is an empty array at first") do
+       expect(Word.all()).to(eq([]))
+     end
+   end
+
+  #  describe('.clear') do
+  #    it ("deletes all of the saved definitions") do
+  #      Definition.new("test_word is a Ruby argument").save()
+  #      Definition.clear()
+  #      expect(Definition.all()).to(eq([]))
+  #    end
+  #  end
+   #
+  #  describe('.find') do
+  #    it("returns a definition by its id number") do
+  #      test_word = Definition.new("test_word is a Ruby argument")
+  #      test_word.save()
+  #      test_word2 = Definition.new("test_word2 is a Ruby argument")
+  #      test_word2.save()
+  #      expect(Definition.find(test_word.id())).to(eq(test_word))
+  #    end
+  #  end
 end
