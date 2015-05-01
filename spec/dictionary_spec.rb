b6336012e5a40750e53ddc58cb8aa055cccc3ea0
chase <student@epicodus.com>
2015-05-01 10:00:46 -0700

    add a definition to the definition class, spec pass

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -5,4 +5,10 @@ require('definition')
 
 describe('Dictionary') do
 
-  describe('')
+  describe('#define') do
+   it('returns the definition of a word') do
+     test_word = Definition.new("test_word is a Ruby argument")
+     expect(test_word.define()).to(eq("test_word is a Ruby argument"))
+   end
+ end
+ end
